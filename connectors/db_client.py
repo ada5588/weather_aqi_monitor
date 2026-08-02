879297fea4a317765f3edca2072317b7a638f6ed
Ada <gcc12289@gmail.com>
2026-08-02 15:46:50 -0400

    add db_client.py

---
+++ connectors/db_client.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+PROFILES = {
+    "readonly": "POSTGRES_READONLY",
+    "write": "POSTGRES_WRITE"
+}
+
+def get_postgres_connection(profile="readonly"):
+    prefix = PROFILES[profile]
+    return psycopg2.connect(
+        host=os.getenv(f"{prefix}_HOST"),
+        port=os.getenv(f"{prefix}_PORT", 5432),
+        dbname=os.getenv(f"{prefix}_DB"),
+        user=os.getenv(f"{prefix}_USER"),
+        password=os.getenv(f"{prefix}_PASSWORD"),
+    )
