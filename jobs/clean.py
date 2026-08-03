558af5865a926c7abe090f1cb7ad47921a4e64e5
Ada <gcc12289@gmail.com>
2026-08-02 22:45:19 -0400

    upadte unpack function to process and combine multiple city data

--- jobs/clean.py
+++ jobs/clean.py
@@ -1,32 +1,38 @@
 import pandas as pd
 import numpy as np
 
-def unpack_weather_response(response):
+def unpack_weather_response(weather_data):
 	# Process hourly data. The order of variables needs to be the same as requested.
-	hourly = response.Hourly()
+	cities = list(weather_data.keys())
+	
+	combined_dataframe = []
+	for i in cities:
+		hourly = weather_data[i].Hourly()
 
-	hourly_data = {
-		"timestamp": pd.date_range(
-			start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
-			end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
-			freq=pd.Timedelta(seconds=hourly.Interval()),
-			inclusive="left",
-		).tz_convert("America/New_York")
-	}
-	hourly_data["temperature"] = hourly.Variables(0).ValuesAsNumpy()
-	hourly_data["relative_humidity"] = hourly.Variables(1).ValuesAsNumpy()
-	hourly_data["apparent_temperature"] = hourly.Variables(2).ValuesAsNumpy()
-	hourly_data["rain"] = hourly.Variables(3).ValuesAsNumpy()
-	hourly_data["precipitation_probability"] = hourly.Variables(4).ValuesAsNumpy()
-	hourly_data["uv_index"] = hourly.Variables(5).ValuesAsNumpy()
+		hourly_data = {
+			"timestamp": pd.date_range(
+				start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+				end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+				freq=pd.Timedelta(seconds=hourly.Interval()),
+				inclusive="left",
+			).tz_convert("America/New_York")
+		}
+		hourly_data["temperature"] = hourly.Variables(0).ValuesAsNumpy()
+		hourly_data["relative_humidity"] = hourly.Variables(1).ValuesAsNumpy()
+		hourly_data["apparent_temperature"] = hourly.Variables(2).ValuesAsNumpy()
+		hourly_data["rain"] = hourly.Variables(3).ValuesAsNumpy()
+		hourly_data["precipitation_probability"] = hourly.Variables(4).ValuesAsNumpy()
+		hourly_data["uv_index"] = hourly.Variables(5).ValuesAsNumpy()
 
-	hourly_dataframe = pd.DataFrame(data=hourly_data)
+		hourly_dataframe = pd.DataFrame(data=hourly_data)
 
-	# Keep only today's America/New_York date
-	today = pd.Timestamp.now(tz="America/New_York").normalize()
-	hourly_dataframe = hourly_dataframe[hourly_dataframe["timestamp"].dt.normalize() == today]
-
-	return hourly_dataframe
+		# Keep only today's America/New_York date
+		today = pd.Timestamp.now(tz="America/New_York").normalize()
+		hourly_data = hourly_dataframe[hourly_dataframe["timestamp"].dt.normalize() == today]
+		hourly_data['city_id'] = i
+		combined_dataframe.append(hourly_data)
+	
+	return pd.concat(combined_dataframe, ignore_index=True)
 
 
 def clean_weather_data(hourly_dataframe):
